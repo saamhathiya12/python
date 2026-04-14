7923e1254f85dab37c14ed9219d385f381be1d0d
saamhathiya12 <saam.hathiya139155@marwadiuniversity.ac.in>
2026-04-14 16:36:59 +0530

    Delete p6 (1).py

--- p6 (1).py
+++
@@ -1,7 +0,0 @@
-# Define a procedure histogram() that takes a list of integers and prints a histogram to the screen. For example, histogram([4, 9, 7]) should print the following:
-def histogram(numbers):
-    for num in numbers:
-
-        print('*' * num)
-
-histogram([4, 9, 7])
